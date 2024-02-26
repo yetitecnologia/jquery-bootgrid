292cfdf74930c79018af8110caf4fa4e5b5a8e1e
Ad Schellevis <ad@opnsense.org>
2024-02-26 20:40:35 +0100

    converters / datetime - when an epoch is offered as number, make sure to convert to moment() before sending it to the client. Adds support for datetime fields as integer/float data.

--- src/public.js
+++ src/public.js
@@ -264,6 +264,9 @@ Grid.defaults = {
               return value ? moment(parseInt(value)*1000) : "";
            },
            to: function (value) {
+              if (!Object.is(value)) {
+                 value = moment(parseInt(value)*1000);
+              }
               return value ? value.format("lll") : "";
            }
         },
